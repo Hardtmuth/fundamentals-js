305c49437e01458f7527f93aef98e62e797832c1
Hardtmuth <abramow-a.v@ya.ru>
2023-12-11 19:10:40 +0300

    add answers

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+const concatStrings = (a, b) => a + b;
+
+const isString = (data) => typeof data === 'string';
+
+const identifySign = (num) => {
+  switch (num) {
+    case 0:
+      return 'Ноль';
+    case num < 0:
+      return 'Отрицательное число';
+    default:
+      return 'Положительное число';
+  }
+};
+
+const reverseWords = (str) => {
+  let result = '';
+  const splittedStr = str.split(' ');
+  for (const word of splittedStr) {
+    result = `${word} ${result}`
+  }
+};
+
+const wordsCount = (str) => str.split(' ').length;
